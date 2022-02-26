afc4ebd3003d31b0b43eaf88191772f2640de8fe
Jig <budmashh@yahoo.com>
2022-02-26 11:14:23 -0800

    boilerplate css

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*var mdiv = document.createElement("div");
-mdiv.style.width = "200px";
-mdiv.style.height = "200px";
-mdiv.style.background = "red";
-mdiv.style.color = "white";
-mdiv.style.display = "flex"
-document.getElementById("main").appendChild(mdiv);
-const newContent = ;
-    document.getElementById('main').appendChild(div)
-class="lo" style="background: pink; min-width: 200px; min-height:200px; border: black 1px solid"*/
-
-
-
-let div = document.createElement("div");
-let container = document.getElementById('main')
-
-for (var i = 1; i <= 16; i++) {
-    document.write('<div class="dives">' + i.toString() + '</div>');
-
-}
+const appContainer = document.querySelector('.app-container');
+const clearButton = document.querySelector('.clear');
+ 
