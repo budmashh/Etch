876268a6291329909139c9da464c43c79febf7e6
Jig <budmashh@yahoo.com>
2022-03-01 00:01:49 -0800

    grid of divs

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const appContainer = document.querySelector('.app-container');
 const clearButton = document.querySelector('.clear');
- 
+ const eraserButton = document.querySelector('.eraser');
+ const rgbButton = document.querySelector('.rgb-color');
+ const greyButton = document.querySelector('.grey');
+ const userColorButton = document.querySelector('.user-color');
+
+ let rgb = false;
+ let eraser = false;
+ let grey = false;
+ let userColor = false;
+
+ function makeDivs(gridNum = 16){
+     let totalDivs = gridNum * gridNum;
+     appContainer.style.gridTemplateColumns =`(repeat ${gridNum}, auto)`;
+     appContainer.style.gridTemplateRows =`(repeat ${gridNum}, auto)`;
+
+     for ( i=1; i<totalDivs; i++){
+         let div = document.createElement('div');
+         div.style.borderStyle = 'dashed';
+         //div.addEventListener('mouseover', changeBackground())
+         appContainer.appendChild(div);
+     }
+ }
+
+ makeDivs();
